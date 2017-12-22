1d19fcf434dacdfcdc0ae6f59dde7f9dc082122f
AnhDT <anhdt@rikkeisoft.com>
2017-12-22 10:18:35 +0700

    validate that all field is not empty before create car, if so send error info

--- models/car.js
+++ models/car.js
@@ -3,16 +3,23 @@ const Schema = mongoose.Schema;
 
 // create ninja Schema & model
 const CarSchema = new Schema({
-    "Ref" : String,
-    "make" : String,
-    "model" : String,
-    "FOB" : Number,
+    "Ref": String,
+    "make": String,
+    "model": String,
+    "FOB": Number,
     "VehicleWidth": Number,
     "VehicleLength": Number,
     "VehicleHeight": Number,
-    "Volume" : Number
+    "Volume": Number
 });
 
+CarSchema.pre('save', function (next) {
+    const x = this
+    console.log(x)
+    next()
+})
 const Car = mongoose.model('cars', CarSchema);
+
+
 
 module.exports = Car;
--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,13 @@ const Car = require('../models/car');
 const Port = require('../models/port');
 
 
-router.get('/test', function (req, res, next) {
+router.get('/test', (req, res, next) => {
   Car.find({})
     .then((data) => res.send(data))
 })
 
 // return cars, 
-router.get('/cars', function (req, res, next) {
+router.get('/cars', (req, res, next) => {
 
   // if query string is not provided, return ''
   var country = req.query.country || '';
@@ -18,30 +18,26 @@ router.get('/cars', function (req, res, next) {
   var port = req.query.port || '';
   var keyword = req.query.keyword || '';
 
-
-
   const target = Port.findOne({ name: port, country: country });
   var cars;
 
   if (!keyword) {
     // if not searching, return all cars
     cars = Car.find({})
-
   } else {
     // if user is searching, return cars that match with keyword
     cars = Car.find({ $text: { $search: keyword } })
-
   }
 
   // wait for both cars and target query complete
   Promise.all([target, cars])
-    .then((result) => {
-
-
-
+    .then(result => {
+      // result[0] is response of target query, result[1] is response of cars query
+      // when result[0] is null that mean user has not been selected a country and port yet, we not going to caculate the price, just return the data
       if (!result[0]) {
         res.send(result);
       } else {
+        // user select a country and port, we caculate the price for that target
         const FreightPerM3 = result[0].FreightPerM3;
         var x = result[1].map((val, key) => {
           var finalPrice = FreightPerM3 * val.Volume + val.FOB;
@@ -57,72 +53,102 @@ router.get('/cars', function (req, res, next) {
         result[1] = x
         res.send(result)
       }
-
     })
 });
 
 
 // return a car with specific id
-router.get('/cars/:id', function (req, res, next) {
-  Car.findById({ _id: req.params.id }).then(function () {
-    Car.findOne({ _id: req.params.id }).then(function (car) {
-      res.send(car);
-    });
-  }).catch(next);
-});
+
+router.get('/cars/:id', async (req, res, next) => {
+  try {
+    let car = await Car.findById({ _id: req.params.id })
+    res.send(car)
+  } catch (error) {
+    next(error)
+  }
+})
+
+
 
 // add a new car to the db
-router.post('/cars', function (req, res, next) {
-  Car.create(req.body).then(function (car) {
+router.post('/cars', (req, res, next) => {
+  let errors = [];
+  for (var key in req.body) {
+    if (req.body.hasOwnProperty(key)) {
+      if (!req.body[key]) {
+        errors.push(key)
+      }
+    }
+  }
+
+  if (errors.length >= 1) {
+    res.send("Please provide enough info")
+    return;
+  }
+
+  Car.create(req.body).then(car => {
     res.send(car);
   }).catch(next);
 });
 
 // update a car in the db
-router.patch('/cars/:id', function (req, res, next) {
-  Car.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
-    Car.findOne({ _id: req.params.id }).then(function (car) {
-      res.send(car);
-    });
-  }).catch(next);
+router.patch('/cars/:id', (req, res, next) => {
+  Car.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then((car) => Car.findOne({ _id: req.params.id })) // car will be the car before update, so we need to find the updated car and send to user
+    .then(updatedCar => res.send(updatedCar))
+    .catch(next);
+  // try {
+  //   let car = await Car.findByIdAndUpdate({ _id: req.params.id }, req.body)
+  //   let updatedCar = await Car.findOne({ _id: req.params.id })
+  //   res.send(updatedCar)
+  // } catch (error) {
+  //   next(error)
+  // }
 });
 
 // delete a car from the db
-router.delete('/cars/:id', function (req, res, next) {
-  Car.findByIdAndRemove({ _id: req.params.id }).then(function (car) {
-    res.send(car);
-  }).catch(next);
+router.delete('/cars/:id', async (req, res, next) => {
+  try {
+    let car = await Car.findByIdAndRemove({ _id: req.params.id })
+    res.send(car)
+  } catch (error) {
+    next(error)
+  }
 });
 
 
-
 // return all countries (distinct)
-router.get('/countries', function (req, res, next) {
-  Port.find({}).distinct('country')
-    .then((countries) => {
-      res.send(countries);
-    })
+router.get('/countries', async (req, res, next) => {
+  try {
+    let countries = await Port.find({}).distinct('country')
+    res.send(countries);
+  } catch (error) {
+    next(error)
+  }
 })
 
 
-
 // return ports belong to a specific country
-router.get('/portByCountry/:country', function (req, res, next) {
+router.get('/portByCountry/:country', async (req, res, next) => {
   var country = req.params.country;
-  Port.find({ country: country })
-    .then(function (result) {
-      res.send(result)
-    })
-    .catch(next)
+  try {
+    let ports = await Port.find({ country: country })
+    res.send(ports)
+  } catch (error) {
+    next(error)
+  }
 })
 
 
 // get all car sorted by model DESC
-router.get('/cars-desc', function (req, res, next) {
-  cars = Car.find({}).sort({ model: -1 })
-    .then(resp => res.send(resp))
+router.get('/cars-desc', async (req, res, next) => {
+  try {
+    let cars = await Car.find({}).sort({ model: -1 })
+    res.send(cars)
+  } catch (error) {
+    next(error)
+  }
 
 })
-
 
 module.exports = router;
